396bbb980e1f15f246f83a8737b93211a5232817
boxxy <91877601+devbo-xy@users.noreply.github.com>
2022-11-23 08:02:14 +0700

    remove typo

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,3 @@
 export { default as Djamboel } from "./logoDjamboel.png";
-export { default as HamburgerBar } from "./HamburgerBar.svg";
+export { default as HamburgerBar } from "./hamburgerBar.svg";
 export { default as CloseBar } from "./CloseBar.svg";
